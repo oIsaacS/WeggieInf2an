8fc15d0e84ded5438209f6635f1796a332b260a2
Isaac Siqueira <rm88714@estudante.fieb.edu.br>
2023-08-23 18:42:03 -0300

    Criação de ambiente de desenvolvimento

---
+++ src/main/java/com/itb/tcc/WeggieInf2an/WeggieInf2anApplication.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.WeggieInf2an;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeggieInf2anApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeggieInf2anApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itb/tcc/WeggieInf2an/WeggieInf2anApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.WeggieInf2an;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeggieInf2anApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
